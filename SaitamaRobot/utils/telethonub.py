87c7f490acfeabcd036acedd68156398b7ea6680
UNKNOWN <84973544+Unknown-San@users.noreply.github.com>
2021-08-14 07:05:22 +0530

    Update telethonub.py

--- SaitamaRobot/utils/telethonub.py
+++ SaitamaRobot/utils/telethonub.py
@@ -3,11 +3,9 @@ import sys
 from telethon import TelegramClient
 from telethon.sessions import StringSession
 
-from SaitamaRobot.utils.conf import get_int_key, get_str_key
-
-STRING_SESSION = get_str_key("STRING_SESSION", required=True)
-API_ID = get_int_key("APP_ID", required=True)
-API_HASH = get_str_key("APP_HASH", required=True)
+STRING_SESSION = "BQAxSFWK7SGbivX5vRcnzmxDI5sD09SwImOfB5DBKa_UYwE0o9DNNTzmrubMkOerE_O39VcBj231AMYSSOVTWZgwUM-smdqoseJDWah1xw7VwdM_pmTmo4gMvaVAwhMYDRus1VvzSjdBuTF4-Rjzc8LANJPPmmP5FCI8flN_Ul4mew1WkGzEIVAUBoQvDs9Vqlwx0xtW_Akc07e1Qdhz_n7sb-S5YfWoE4oC8gtXwDIBli-s8MuP0nyarFotea66Aso3tV4yN5J6EAKddAPGC3bJ7TRS6ckjlduNroJvSWL6QJ_neGzmYGv0X4hKLXwPAhGBmFtWx9sEeA1r_qO32Pj4a21KgQA"
+API_ID = 4161078
+API_HASH = "005968599cf426c7cefe134e1b5a91d6"
 
 ubot = TelegramClient(StringSession(STRING_SESSION), API_ID, API_HASH)
 try:
