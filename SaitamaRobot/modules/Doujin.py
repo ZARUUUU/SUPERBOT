ec3d9aa12cf807fcbecdd852496ad395fe008bea
UNKNOWN <84973544+Unknown-San@users.noreply.github.com>
2021-08-22 07:02:33 +0530

    Update Doujin.py

--- SaitamaRobot/modules/Doujin.py
+++ SaitamaRobot/modules/Doujin.py
@@ -29,6 +29,8 @@ def hentai(_,message):
         file_paths.sort(key=lambda f: int(re.sub('\D', '', f)))
         with open(f"{hmm}.pdf" ,"wb") as f:
             f.write(img2pdf.convert(file_paths)) 
-        pbot.send_document(message.chat.id , f"{hmm}.pdf")
-        os.remove(f"{hmm}.pdf")
-        os.system(f"rm -rf {hmm}")
+       
+
+    pbot.send_document(message.chat.id , f"{hmm}.pdf")
+    os.remove(f"{hmm}.pdf")
+    os.system(f"rm -rf {hmm}")
