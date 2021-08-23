4a70270e7b086e06936ed06de176d6dfa3612561
UNKNOWN <84973544+Unknown-San@users.noreply.github.com>
2021-08-23 18:28:04 +0530

    Update imgeditor.py

--- SaitamaRobot/modules/imgeditor.py
+++ SaitamaRobot/modules/imgeditor.py
@@ -7,7 +7,7 @@ from pyrogram.types import (
 )
 
 # By @TroJanzHEX
-from SaitamaRobot.utils.resources.ImageEditor.edit_1 import (  # pylint:disable=import-error
+from SaitamaRobot.utils.ImageEditor.edit_1 import (  # pylint:disable=import-error
     black_white,
     box_blur,
     bright,
@@ -15,7 +15,7 @@ from SaitamaRobot.utils.resources.ImageEditor.edit_1 import (  # pylint:disable=
     mix,
     normal_blur,
 )
-from SaitamaRobot.utils.resources.ImageEditor.edit_2 import (  # pylint:disable=import-error
+from SaitamaRobot.utils.ImageEditor.edit_2 import (  # pylint:disable=import-error
     cartoon,
     circle_with_bg,
     circle_without_bg,
@@ -25,13 +25,13 @@ from SaitamaRobot.utils.resources.ImageEditor.edit_2 import (  # pylint:disable=
     sepia_mode,
     sticker,
 )
-from SaitamaRobot.utils.resources.ImageEditor.edit_3 import (  # pylint:disable=import-error
+from SaitamaRobot.utils.ImageEditor.edit_3 import (  # pylint:disable=import-error
     black_border,
     blue_border,
     green_border,
     red_border,
 )
-from SaitamaRobot.utils.resources.ImageEditor.edit_4 import (  # pylint:disable=import-error
+from SaitamaRobot.utils.ImageEditor.edit_4 import (  # pylint:disable=import-error
     inverted,
     removebg_plain,
     removebg_sticker,
@@ -41,7 +41,7 @@ from SaitamaRobot.utils.resources.ImageEditor.edit_4 import (  # pylint:disable=
     rotate_270,
     round_sticker,
 )
-from SaitamaRobot.utils.resources.ImageEditor.edit_5 import (  # pylint:disable=import-error
+from SaitamaRobot.utils.ImageEditor.edit_5 import (  # pylint:disable=import-error
     normalglitch_1,
     normalglitch_2,
     normalglitch_3,
