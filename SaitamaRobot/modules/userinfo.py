47ab4255dcbfee3999d62540c81491171de83f82
UNKNOWN <84973544+Unknown-San@users.noreply.github.com>
2021-08-25 17:54:43 +0530

    Update userinfo.py

--- SaitamaRobot/modules/userinfo.py
+++ SaitamaRobot/modules/userinfo.py
@@ -224,7 +224,7 @@ def info(update: Update, context: CallbackContext):
         return
 
     rep = message.reply_text(
-        "<code>Checking History In My Jujutsu Database...</code>", parse_mode=ParseMode.HTML)
+        "<code>Appraising...</code>", parse_mode=ParseMode.HTML)
 
     text = (f"╒═══「<b> Appraisal results:</b> 」\n"
             f"ID: <code>{user.id}</code>\n"
